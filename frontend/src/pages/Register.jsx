3694eabdc066fa73bd0fd744140edaa9c0821fc4
Chidozie Nnaduruaku <nchidozien@gmail.com>
2022-11-05 11:11:24 -0400

    Implement login and logout

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -42,7 +42,7 @@ function Register() {
 	const onSubmit = (e) => {
 		e.preventDefault()
 
-		if (password != password2) {
+		if (password !== password2) {
 			toast.error('passwords do not match')
 		} else {
 			const userData = { name, email, password }
